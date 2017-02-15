5514256fd2c12609e4b1cbdf8193c9629f59de4e
Noim <nilsbergmann@noim.io>
2017-02-15 21:20:11 +0100

    Update

--- t.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by nilsbergmann on 15.02.17.
- */
-const DSB = require('./index');
-const path = require('path');
-const dsb = new DSB(326781, 'MCG-Plan', path.join(__dirname, 'cache.json'));
-dsb.getData().then(Data => {
-    console.log(JSON.stringify(Data));
-}).catch(console.error);
