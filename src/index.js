91b90c49f8d51df955fa020c6c79342dab6e6b75
Nils <magiccraft.nitrado@gmail.com>
2019-12-02 21:40:53 +0100

    Heineking... I love you guys

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import Encode from './DSBEncoding';
 import Decode from './DSBDecode';
 import percentage from 'percentage-calc';
-import { URL } from 'url';
-import cheerio from 'cheerio';
-import qs from 'querystring';
 
 /**
  * Main Library class
@@ -40,11 +37,7 @@ export default class DSB {
 		 * @private
 		 */
 		this.urls = {
-			login: 'https://www.dsbmobile.de/Login.aspx',
-			main: 'https://www.dsbmobile.de/',
-			Data: null,
-			default: 'https://www.dsbmobile.de/default.aspx',
-			Configuration: 'https://www.dsbmobile.de/scripts/configuration.js'
+			Data: 'https://app.dsbcontrol.de/JsonHandler.ashx/GetData'
 		};
 		/**
 		 * @private
@@ -54,7 +47,7 @@ export default class DSB {
 		 * @private
 		 */
 		this.axios.defaults.headers.common['User-Agent'] =
-			'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.32 Safari/537.36';
+			'DSBmobile/9759 (iPhone; iOS 13.2.2; Scale/3.00)';
 		if (cache) {
 			/**
 			 * @private
@@ -74,38 +67,28 @@ export default class DSB {
 	 * @returns {Promise.<Object>}
 	 */
 	async fetch(progress = () => {}) {
-		const cookies = await this._getSession(progress);
-		await this._loadDataUrl(progress);
 		// Progress State: 3
 		const response = await this.axios({
 			method: 'POST',
 			data: {
 				req: {
 					Data: Encode({
-						UserId: '',
-						UserPw: '',
-						Abos: [],
-						AppVersion: '2.3',
-						Language: 'de',
-						AppId: '',
-						Device: 'WebApp',
 						PushId: '',
-						BundleId: 'de.heinekingmedia.inhouse.dsbmobile.web',
+						UserId: this.username,
+						UserPw: this.password,
+						Device: 'iPhone',
+						AppVersion: '2.5.6',
+						Language: 'en-DE',
 						Date: new Date(),
+						BundleId: 'de.digitales-schwarzes-brett.dsblight',
+						OsVersion: '13.2.2',
 						LastUpdate: new Date(),
-						OsVersion:
-							'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'
+						AppId: 'BC86F8E5-5D4A-4A19-A317-04D1E52FF9ED'
 					}),
 					DataType: 1
 				}
 			},
 			url: this.urls.Data,
-			headers: {
-				Bundle_ID: 'de.heinekingmedia.inhouse.dsbmobile.web',
-				Referer: this.urls.main,
-				Cookie: cookies,
-				'X-Requested-With': 'XMLHttpRequest'
-			},
 			onUploadProgress(e) {
 				console.log(JSON.stringify(e));
 			},
@@ -118,164 +101,6 @@ export default class DSB {
 		const decoded = Decode(response.data.d);
 		progress(percentage.from(5, 5));
 		return decoded;
-	}
-
-	/**
-	 * Login with username and password
-	 * @param {String|Number} [username=this.username]
-	 * @param {String|Number} [password=this.password]
-	 * @returns {Promise.<String>}
-	 * @private
-	 */
-	async _login(username = this.username, password = this.password) {
-		const loginFormResponse = await this.axios({
-			method: 'GET',
-			url: this.urls.login
-		});
-
-		const html = loginFormResponse.data;
-
-		const $ = cheerio.load(html);
-
-		const __VIEWSTATE = $('input[name=__VIEWSTATE]').attr('value');
-		const __VIEWSTATEGENERATOR = $('input[name=__VIEWSTATEGENERATOR]').attr(
-			'value'
-		);
-		const __EVENTVALIDATION = $('input[name=__EVENTVALIDATION]').attr(
-			'value'
-		);
-
-		const bodyData = {
-			txtUser: username,
-			txtPass: password,
-			ctl03: 'Anmelden',
-			__EVENTVALIDATION,
-			__VIEWSTATE,
-			__VIEWSTATEGENERATOR
-		};
-
-		const body = qs.stringify(bodyData);
-
-		const response = await this.axios({
-			method: 'POST',
-			url: this.urls.login,
-			data: body,
-			headers: {
-				'Content-Type': 'application/x-www-form-urlencoded'
-			},
-			validateStatus(status) {
-				return status === 200 || status === 302;
-			},
-			maxRedirects: 0,
-			onUploadProgress(e) {
-				console.log(JSON.stringify(e));
-			},
-			onDownloadProgress(e) {
-				console.log(JSON.stringify(e));
-			}
-		});
-		if (!response.headers['set-cookie'])
-			throw new Error('Login failed. Returned no cookies.');
-
-		this.cookies = response.headers['set-cookie'].join('; ');
-
-		if (!this.cookies.indexOf('DSBmobile=') < 0)
-			throw new Error('Missing DSBmobile cookie. Login failed.');
-
-		return this.cookies;
-	}
-
-	/**
-	 * Checks if dsb session cookie is valid
-	 * @param {String} [cookies=this.cookies]
-	 * @returns {Promise.<boolean>}
-	 * @private
-	 */
-	async _checkCookies(cookies = this.cookies) {
-		let returnValue = false;
-		try {
-			returnValue = !!(await this.axios({
-				method: 'GET',
-				url: this.urls.default,
-				validateStatus(status) {
-					return status === 200;
-				},
-				maxRedirects: 0,
-				headers: {
-					Cookie: cookies,
-					'Cache-Control': 'no-cache',
-					Pragma: 'no-cache'
-				}
-			}));
-		} catch (e) {
-			return false;
-		} finally {
-			return returnValue;
-		}
-	}
-
-	async _loadDataUrl(progress) {
-		if (this.urls.Data) return;
-		const cookies = await this._getSession(progress);
-		const response = await this.axios({
-			method: 'GET',
-			url: this.urls.Configuration,
-			headers: {
-				Bundle_ID: 'de.heinekingmedia.inhouse.dsbmobile.web',
-				Referer: this.urls.main,
-				Cookie: cookies,
-				'X-Requested-With': 'XMLHttpRequest'
-			},
-			onUploadProgress(e) {
-				console.log(JSON.stringify(e));
-			},
-			onDownloadProgress(e) {
-				console.log(JSON.stringify(e));
-			}
-		});
-
-		const URLRegex = /METHOD\s*:\s*["'](.*)["']/gm;
-		const matches = URLRegex.exec(response.data);
-		if (matches.length < 2) throw new Error('Can not find data url.');
-		const dataURLPath = matches[1];
-		const url = new URL(dataURLPath, this.urls.main);
-		this.urls.Data = url.toString();
-	}
-
-	/**
-	 * Get a valid session
-	 * @param {Function} [progress]
-	 * @returns {Promise.<String>}
-	 * @private
-	 */
-	async _getSession(progress = () => {}) {
-		let returnValue;
-		try {
-			const cookies = this.cookies
-				? this.cookies
-				: await this.cache.get();
-			progress(percentage.from(1, 5));
-			if (await this._checkCookies(cookies)) {
-				returnValue = cookies;
-				progress(percentage.from(3, 5));
-			} else {
-				returnValue = await this._login();
-				progress(percentage.from(2, 5));
-				this.cache
-					? await this.cache.set(returnValue)
-					: (this.cookies = returnValue);
-				progress(percentage.from(3, 5));
-			}
-		} catch (e) {
-			returnValue = await this._login();
-			progress(percentage.from(2, 5));
-			this.cache
-				? await this.cache.set(returnValue)
-				: (this.cookies = returnValue);
-			progress(percentage.from(3, 5));
-		} finally {
-			return returnValue;
-		}
 	}
 
 	/**
@@ -334,83 +159,3 @@ export default class DSB {
 
 DSB.Decode = Decode;
 DSB.Encode = Encode;
-
-class DSBSessionStorageManager {
-	/**
-	 * Class to store the dsb session
-	 * @param [path=""]
-	 * @param [cookies=""]
-	 */
-	constructor(path = '', cookies = '') {
-		this.path = path;
-		this.cookies = cookies;
-		this.fs = DSBSessionStorageManager.isNode() ? require('fs') : undefined;
-	}
-
-	/**
-	 * Retrieves session from cache.
-	 * @returns {Promise.<String>}
-	 */
-	async get() {
-		if (DSBSessionStorageManager.isNode()) {
-			this.cookies = await new Promise((resolve, reject) => {
-				this.fs.readFile(this.path, (err, data) => {
-					if (err) return reject(err);
-					if (typeof data !== 'string') {
-						let value;
-						try {
-							value = data.toString();
-						} catch (e) {
-							return reject(e);
-						} finally {
-							return resolve(value);
-						}
-					} else {
-						return resolve(data);
-					}
-				});
-			});
-			return this.cookies;
-		} else {
-			if (window.localStorage) {
-				return window.localStorage.getItem('DSBSession');
-			} else {
-				return this.cookies;
-			}
-		}
-	}
-
-	/**
-	 * Sets the session in the cache.
-	 * @param value
-	 * @returns {Promise.<void>}
-	 */
-	async set(value = '') {
-		this.cookies = value;
-		if (DSBSessionStorageManager.isNode()) {
-			try {
-				await new Promise((resolve, reject) => {
-					this.fs.writeFile(this.path, value, err => {
-						if (err) return reject(err);
-						return resolve();
-					});
-				});
-			} catch (e) {}
-		} else {
-			if (window.localStorage) {
-				return window.localStorage.setItem('DSBSession', value);
-			}
-		}
-	}
-
-	/**
-	 * Checks if this module is running in a browser env or node env.
-	 * @returns {boolean}
-	 */
-	static isNode() {
-		return (
-			Object.prototype.toString.call(global.process) ===
-			'[object process]'
-		);
-	}
-}
